b41abe4c1e845ebee8e883195582ca55a79e8372
Lauri <huhta.lauri@gmail.com>
2020-09-13 19:17:54 +0300

    Ticket count bar chart now and graphs printing to pdf

--- scripts/daily_reports.r
+++ scripts/daily_reports.r
@@ -2,30 +2,34 @@ suppressMessages(library(tidyverse))
 suppressMessages(library(ggplot2))
 suppressMessages(library(plyr))
 suppressMessages(library(plotly))
+suppressMessages(library(gridExtra))
 
 options(scipen=9999) # Disables scientific notation
 
 setwd('~/repos/RReporting')
 cwd <- getwd()
 csvpath <- paste(cwd, "/scripts/data/jira_tickets.csv", sep = '')
-
 data <- suppressMessages(read_csv(csvpath))
 
 # Changing the order of the data to suit better for Cumulative Flow Diagram
 cfdLevels <- c("Backlog", "Selected for Development", "In Progress", "Closed", "Done")
 data$status <- factor(data$status,levels = cfdLevels)
-## Cumulative Flow Diagram ##
-cumplot_pre <- ggplot(data, aes(x = currentdate, fill = status))
-cumplot_pre + geom_bar(aes(y = ..count.., text = paste('Date: ', as.Date(currentdate), '\n',
-                                                      'Status: ', status)))+
-              ggtitle("Cumulative Flow Diagram")+
-              theme(axis.title.x = element_blank())+
-              ylab("Count of Tickets")+
-              scale_fill_manual(values = c("#2eb9d1", "#7f1dcf", "#fc3903", "#a6948f", "#fc9d03")) -> cumplot
-# Interactive Plotly plot
-cumplot_plotly <- ggplotly(cumplot, tooltip = c("count", "text"))
 
-cumplot_plotly
+
+## Cumulative Flow Diagram ##
+data %>% 
+  group_by(currentdate, status) %>% 
+  summarise(count = length(status)) %>%
+  ggplot(aes(x = currentdate, y = count, fill = status)) +
+  geom_col(position = position_stack()) +
+  ggtitle("Cumulative Flow Diagram")+
+  theme(axis.title.x = element_blank())+
+  ylab("Count of Tickets")+
+  scale_fill_manual(values = c("#2eb9d1", "#7f1dcf", "#fc3903", 
+                               "#a6948f", "#fc9d03")) -> cumplot
+
+cumplot_plotly <- ggplotly(cumplot)
+
 
 ## Percent Stacked Barchart ##
 data %>% select(currentdate, status) %>% 
@@ -34,18 +38,38 @@ data %>% select(currentdate, status) %>%
   geom_bar(aes(y = ..count.., text = paste('Date: ', as.Date(currentdate), '\n',
                                            'Status: ', status)), position = "fill") -> psb_pre
 
-psb_plotly <- ggplotly(psb_pre, tooltip = c("text"))
-psb_plotly
+psb_plotly <- ggplotly(psb_pre, tooltip = "text")
+
 
 ## Area Chart ""
-ggplot(data, aes(x = currentdate, y = status, fill = status))+
-  geom_area()
+data %>% select(currentdate, status) %>% 
+  filter(currentdate > Sys.Date() - 120) %>% 
+  ggplot(aes(currentdate, fill = status))+
+  geom_area(stat = "count") -> areachart
 
-data %>% 
-  select(currentdate, status) %>% 
-  group_by(currentdate) %>% 
-  table() -> new_data
+areachart_plotly <- ggplotly(areachart)
 
-new_data
 
-new_df <- as.data.frame(new_data)
+## Tickets by project
+data %>%
+  filter(currentdate == Sys.Date() - 1) %>% 
+  group_by(project_key) %>% 
+  summarise(ticket_count = length(project_key)) %>%
+  ggplot(aes(reorder(project_key, -ticket_count), ticket_count, fill = project_key))+
+    geom_bar(stat = "identity") -> ticketCount_barplot
+
+
+
+
+## Save files to pdf
+
+create_reports <- function() {
+  pdf("reports.pdf")
+  print(cumplot)     # Plot 1 --> in the first page of PDF
+  print(psb_pre) # Plot 2 ---> in the second page of the PDF
+  print(areachart)
+  print(ticketCount_barplot)
+  dev.off() 
+}
+
+create_reports()
